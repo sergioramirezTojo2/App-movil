398d5f67dae156ede75c97c35775a1b32cee72cc
radarodriguezcarlos-glitch <radarodriguezcarlos@gmail.com>
2025-12-16 23:35:53 -0400

    Update map_screen.dart
    git commit -m "feat: implementación RF02 carga inicial de puntos desde backend"
    git commit -m "feat: visualización de POI como marcadores en Google Maps"
    git commit -m "fix: implementación RNF03 manejo de errores y confiabilidad"
    git commit -m "test: pruebas de integración frontend-backend"

--- maps_app/lib/map_screen.dart
+++ maps_app/lib/map_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'services/api_service.dart';
-
+ 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
 
